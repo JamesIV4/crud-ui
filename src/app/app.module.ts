f81605f9a333962291b49cf80fe750e7a87cff57
James Pound <jpound@webmd.net>
2021-09-02 15:40:25 -0400

    Added form validation, date picker, and proper date handling to dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { ListViewComponent } from './list-view/list-view.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
 import { MatInputModule } from "@angular/material/input";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { HttpClientModule } from '@angular/common/http';
 import { NewEntryDialogComponent } from './new-entry-dialog/new-entry-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -33,7 +35,10 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatInputModule,
     MatDialogModule,
     FormsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-entry-dialog/new-entry-dialog.component.ts
+++ src/app/new-entry-dialog/new-entry-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { User } from '../database.service';
+import { FormControl, Validators } from '@angular/forms';
 
 export interface DialogUserData {
     existing: boolean,
@@ -19,9 +19,29 @@ export interface DialogUserData {
     styleUrls: ['./new-entry-dialog.component.scss']
 })
 export class NewEntryDialogComponent {
+    email = new FormControl('',[
+        Validators.email
+    ]);
+    maxDate: Date = new Date;
+    minDate: Date = new Date(new Date().setFullYear(new Date().getFullYear() - 120));
+
     constructor(
         public dialogRef: MatDialogRef<NewEntryDialogComponent>,
         @Inject(MAT_DIALOG_DATA) public data: DialogUserData) { }
+
+    getEmailErrorMessage() {
+        return this.email.hasError('email') ? 'Not a valid email' : '';
+    }
+
+    isValid() {
+        if (!this.data.user.name ||
+            !this.data.user.email ||
+            this.email.errors) {
+            return false
+        }
+
+        return true
+    }
 
     onNoClick() {
         this.dialogRef.close({
@@ -30,6 +50,9 @@ export class NewEntryDialogComponent {
     }
 
     onCloseConfirm() {
+        if (this.data.user.dob) {
+            this.data.user.dob = new Date(this.data.user.dob).toISOString();
+        }
         this.dialogRef.close({
             confirm: true,
             data: this.data
