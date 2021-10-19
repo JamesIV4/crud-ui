c021af80f7c8f25d9335d2177964984592659258
James Pound <james.pound@genesys.com>
2021-10-19 09:42:38 -0400

    Fixed use of any in the applyFilter menthod, proper typing there now. Also Prettier ran on that file

--- src/app/list-view/list-view.component.ts
+++ src/app/list-view/list-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTable } from '@angular/material/table';
 import { PageEvent } from '@angular/material/paginator';
 import { Sort } from '@angular/material/sort';
-import { DatabaseService } from "../database.service";
+import { DatabaseService } from '../database.service';
 import { MatDialog } from '@angular/material/dialog';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatButton } from '@angular/material/button';
@@ -12,17 +12,13 @@ import { User } from '../app.models';
 @Component({
 	selector: 'app-list-view',
 	templateUrl: './list-view.component.html',
-	styleUrls: ['./list-view.component.scss']
+	styleUrls: ['./list-view.component.scss'],
 })
-
 export class ListViewComponent implements OnInit {
 	displayedColumns: string[] = ['id', 'avatar', 'name', 'email', 'dob'];
 	selection = new SelectionModel<User>(false); // Passing allowMultiSelect = false here
 
-	constructor(
-		public database: DatabaseService,
-		public dialog: MatDialog
-		) {}
+	constructor(public database: DatabaseService, public dialog: MatDialog) {}
 
 	@ViewChild(MatTable) table: MatTable<User>;
 
@@ -33,7 +29,10 @@ export class ListViewComponent implements OnInit {
 	selectRow(row: User, event?: any, buttonRef?: MatButton) {
 		// Handle keyboard input to trigger selection
 		if (event) {
-			if (event.key.toLowerCase() === 'enter' || event.code.toLowerCase() === 'space') {
+			if (
+				event.key.toLowerCase() === 'enter' ||
+				event.code.toLowerCase() === 'space'
+			) {
 				this.toggleSelection(row);
 
 				setTimeout(() => {
@@ -42,7 +41,8 @@ export class ListViewComponent implements OnInit {
 					}
 				}, 20);
 			}
-		} else { // If it's a normal click
+		} else {
+			// If it's a normal click
 			this.toggleSelection(row);
 		}
 	}
@@ -50,7 +50,11 @@ export class ListViewComponent implements OnInit {
 	toggleSelection(row: User) {
 		this.selection.toggle(row);
 
-		console.log(`${this.selection.hasValue() ? 'Selected' : 'Deselected'} row${this.selection.hasValue() ? ':\n' + JSON.stringify(row) : '.'}`);
+		console.log(
+			`${this.selection.hasValue() ? 'Selected' : 'Deselected'} row${
+				this.selection.hasValue() ? ':\n' + JSON.stringify(row) : '.'
+			}`
+		);
 	}
 
 	toggleFiltering(focusOn: HTMLInputElement) {
@@ -63,10 +67,12 @@ export class ListViewComponent implements OnInit {
 		}
 	}
 
-	applyFilter(event: any) {
+	applyFilter(event: KeyboardEvent) {
 		// Enter is pressed, set filter query parameter and fetch data
 		if (event.key.toLowerCase() === 'enter') {
-			this.database.filtering.query = event.target.value;
+			this.database.filtering.query = (
+				event.target as HTMLInputElement
+			).value;
 			this.database.fetchData();
 		}
 	}
@@ -82,23 +88,28 @@ export class ListViewComponent implements OnInit {
 					name: newUser ? '' : this.selection.selected[0].name,
 					avatar: newUser ? '' : this.selection.selected[0].avatar,
 					email: newUser ? '' : this.selection.selected[0].email,
-					dob: newUser ? '' : this.selection.selected[0].dob
-				}
-			}
+					dob: newUser ? '' : this.selection.selected[0].dob,
+				},
+			},
 		});
 
-		dialogRef.afterClosed().subscribe(result => {
+		dialogRef.afterClosed().subscribe((result) => {
 			console.log('The dialog was closed');
 
-			if (result.confirm === true) { // If confirm was clicked
+			if (result.confirm === true) {
+				// If confirm was clicked
 				console.log(result.data.user);
 
 				if (newUser) {
 					console.log('Creating new user');
 					this.database.sendRequest('POST', result.data.user);
 				} else {
-					this.selection.clear() // Deselect the user
-					this.database.sendRequest('PUT', result.data.user, result.data.user.id);
+					this.selection.clear(); // Deselect the user
+					this.database.sendRequest(
+						'PUT',
+						result.data.user,
+						result.data.user.id
+					);
 				}
 			}
 		});
@@ -106,19 +117,21 @@ export class ListViewComponent implements OnInit {
 
 	deleteUser(id: any) {
 		this.database.sendRequest('DELETE', {}, id);
-		this.selection.clear() // Deselect the user
+		this.selection.clear(); // Deselect the user
 	}
 
 	convertDate(_date: string) {
 		if (_date) {
 			const date = new Date(_date);
-			return `${date.toLocaleString('default', { month: 'long' })} ${date.getDate()}, ${date.getFullYear()}`;
+			return `${date.toLocaleString('default', {
+				month: 'long',
+			})} ${date.getDate()}, ${date.getFullYear()}`;
 		} else return '';
 	}
 
 	onChangePage(pageEvent: PageEvent) {
 		// Clear any selections
-		this.selection.clear()
+		this.selection.clear();
 
 		// Pass page info for fetch call parameters
 		this.database.pageNum = pageEvent.pageIndex + 1;
@@ -127,7 +140,8 @@ export class ListViewComponent implements OnInit {
 	}
 
 	sortData(sort: Sort) {
-		if (sort.direction === '') { // sort.direction is blank if no sorting is active
+		if (sort.direction === '') {
+			// sort.direction is blank if no sorting is active
 			this.database.sort.sorting = false;
 		} else {
 			this.database.sort.sorting = true;
